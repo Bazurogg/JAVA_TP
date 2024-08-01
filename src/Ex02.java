9f43af0ee57ddfa1e56b55832e4f15677d7ac813
Bazurogg <bazurogg@gmail.com>
2024-08-01 15:48:38 +0200

    JAVA -> Exo02 -> setup Class

--- src/Ex02.java
+++ src/Ex02.java
@@ -5,5 +5,46 @@ import java.util.Scanner;
 
 public class Ex02 {
 
+    public static void mainExo02(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        boolean confirmationX = false;
+        boolean confirmationY = false;
+
+        int X = 0;
+        int Y = 0;
+
+        while (!confirmationX) {
+
+            System.out.println("Veuillez saisir un nombre entier positif pour \"X\" :");
+
+            X = userInput.nextInt();
+
+            System.out.println("Vous avez saisi le nombre : " + X + ". Est-ce correct ? (oui/non)");
+
+            String reponse = userInput.next();
+
+            if (reponse.equalsIgnoreCase("oui")) {
+
+                confirmationX = true;
+
+                System.out.println("Merci pour votre confirmation. \"X\" = " + X);
+
+            } else if (reponse.equalsIgnoreCase("non")) {
+
+                confirmationX = false;
+
+            } else {
+
+                System.out.println("Réponse invalide. Veuillez répondre par 'oui' ou 'non'.");
+
+            }
+
+        }
+
+        userInput.close();
+
+    }
 
 }
