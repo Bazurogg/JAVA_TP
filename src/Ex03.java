d38fa931609752a973f5c6867d1919b3b3d08e6d
Bazurogg <bazurogg@gmail.com>
2024-08-02 09:08:05 +0200

    JAVA -> Exo03 -> Array test + instructions step

--- src/Ex03.java
+++ src/Ex03.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex03 {
 
+    // déclaration d'un tableau.
     public static void mainExo03() {
 
+        //construct avec affectation des données du tableau.
+        int[] intArr1 = new int[] {3, 4, 5};
+
+        // display du tableau
+        System.out.print(Arrays.toString(intArr1));
 
     }
+
+    // execution
+    public static void main(String[] args) {
+
+        mainExo03();
+
+    }
+
 }
